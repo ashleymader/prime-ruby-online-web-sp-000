bc6b3bd50ab29843ef03bea905b0a949bf198cbd
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-25 20:52:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+    if number < 1 | (number % 2) == 0
+      return false 
+    elsif
+      range = (2..number-1).to_a
+      range.none? do |test_num|
+        number % test_num != 0
+        return true
+      end
+    else 
+      true
+    end
+end
+    
