134fec62c4f94d4685c726738a75d9cacf895cfa
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-25 21:00:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number)
-    if number <= 1 || (number % 2) == 0
+    if number < 1 || (number % 2) == 0
       return false 
     elsif
       range = (2..number-1).to_a
